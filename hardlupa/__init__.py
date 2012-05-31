8d26a6bd4f03658da84150cd842feb6beccfb5da
Philip Horger <campadrenalin@gmail.com>
2012-05-30 23:31:28 -0700

    Partway finished, need HardRuntime.globals to test

--- hardlupa/__init__.py
+++ hardlupa/__init__.py
@@ -1,6 +1,13 @@
 import lupa
 import multiprocessing
 
+safe_lua_modules = [
+    "table",
+    "string",
+    "math",
+    "collectgarbage",
+]
+
 class HardRuntime(object):
     def __init__(self):
         self.parent_conn, self.child_conn = multiprocessing.Pipe()
@@ -15,9 +22,14 @@ class HardRuntime(object):
 
     def run(self, conn):
         lua = lupa.LuaRuntime()
+        globalflush(lua)
+        globs = lua.globals()
         while True:
             try:
-                recv(conn, lua)
+                result = recv(conn, lua, globs)
+                if type(result) == tuple:
+                    if result[0] == "globals_update":
+                        globs = result[1]
             except Exception as e:
                 conn.send(e)
         print "HardRuntime sandbox is exiting..."
@@ -47,7 +59,7 @@ class HardRuntime(object):
         return self.call(("exec", code), timeout)
 
 # Receive a request in the remote process
-def recv(conn, lua):
+def recv(conn, lua, globs):
     action, details = conn.recv()
     if not isinstance(details, basestring):
         raise TypeError("Action details should be a string. Got %r" % details)
@@ -55,5 +67,21 @@ def recv(conn, lua):
         conn.send(lua.eval(details))
     elif action=="exec":
         conn.send(lua.execute(details))
+    elif action=="globals_update":
+        globalflush(lua, details[1], details[2])
+        return ("globals_update", lua.globals())
     else:
         raise ValueError("Unknown action type %r" % action)
+
+# Save and/or restore the state of the globals in the Lupa interpreter
+def globalflush(lua, names=safe_lua_modules, values={}):
+    lua_globals = lua.globals()
+    if not values:
+        for x in lua_globals:
+            values[x] = lua_globals[x]
+
+    for x in lua_globals:
+        if x in values:
+            lua_globals[x] = values[x]
+        else:
+            lua_globals[x] = None
